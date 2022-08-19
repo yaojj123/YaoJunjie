ce8ad3adc99de37955f2156c018d222eef53fc4c
Yao Junjie <641284522@qq.com>
2022-08-19 09:00:36 +0800

    第四章作业

---
+++ chapter04/Homework01.java
@@ -0,0 +1,13 @@
+public class Homework01{
+	public static void main(String[] args) {
+		//1.3    2    0   -1.5 
+		//2.134 
+		//3.C D 
+		//4.String ×ª double
+		String str = "18.8";
+		double  d = Double.parseDouble(str);
+		char c = 'º«';
+		String  str2 = c + "";
+		System.out.println(d + str2);
+	}
+} 
